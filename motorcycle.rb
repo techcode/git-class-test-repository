f6eaf92bffacdb2463366830a87730222b38851d
ed <techcode@ymail.com>
2009-02-26 22:35:37 -0800

    Adding new motorcycle.rb class

---
+++ motorcycle.rb
@@ -0,0 +1,16 @@
+class Motorcycle
+  def initialize(make,color)
+    #Instance variables
+    @make=make
+    @color=color
+  end
+  def startEngine
+    if (@engineState)
+      puts 'Engine is already Running'
+    else
+      @engineState=true
+      puts 'Engine Idle'
+    end
+  end
+end
+
